16d102cf28a9edc9fe750683d48afa4737c88c24
Jedrzej <jbszczepaniak@gmail.com>
2017-09-19 22:04:44 +0200

    Implement logout

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 from . import views
 
@@ -8,4 +9,7 @@ app_name = 'core'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'login/$', auth_views.login, name='login'),
+    url(r'logout/$', auth_views.logout, {'next_page': reverse_lazy('core:index')}, name='logout'),
+    # url(r'register/$', views.signup, name='signup'),
+
 ]
