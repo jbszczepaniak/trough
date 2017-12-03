59aff625b012a39c3fad291c201be296e395f935
Jedrzej <jbszczepaniak@gmail.com>
2017-12-03 19:51:29 +0100

    Add ingredient model

--- trough/main/models.py
+++ trough/main/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Dish(models.Model):
     name = models.CharField(max_length=255, blank=False)
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255, blank=False)
--- trough/main/tests.py
+++ trough/main/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from trough.main.models import Dish
+from trough.main.models import Dish, Ingredient
 
 
 class TestDishModel(TestCase):
@@ -12,3 +12,13 @@ class TestDishModel(TestCase):
         self.dish.save()
         new_count = Dish.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+
+class TestIngredientModel(TestCase):
+    def setUp(self):
+        self.ingredient_name = 'tuna'
+        self.ingredient = Ingredient(name=self.ingredient_name)
+
+    def test_model_can_create_an_ingredient(self):
+        self.ingredient.save()
+        Ingredient.objects.get(name=self.ingredient_name)
