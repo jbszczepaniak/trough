2c745b513e980440428927ea98cce590bb54e057
Jedrzej <jbszczepaniak@gmail.com>
2017-12-04 22:12:51 +0100

    Add ingredient-dish relationship

--- trough/main/models.py
+++ trough/main/models.py
@@ -7,3 +7,4 @@ class Dish(models.Model):
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255, blank=False)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE, default=None, null=True)
--- trough/main/tests/model_test.py
+++ trough/main/tests/model_test.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+import pytest
 from trough.main.models import Dish, Ingredient
 
 
@@ -22,3 +22,11 @@ class TestIngredientModel(TestCase):
     def test_model_can_create_an_ingredient(self):
         self.ingredient.save()
         Ingredient.objects.get(name=self.ingredient_name)
+
+
+@pytest.mark.django_db
+def test_dish_can_have_many_ingredients():
+    tuna_with_chips = Dish(name='tuna_with_chips')
+    tuna_with_chips.save()
+    tuna_with_chips.ingredient_set.create(name='tuna')
+    tuna_with_chips.ingredient_set.create(name='chips')
