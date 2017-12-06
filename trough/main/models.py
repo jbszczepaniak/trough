5f9f8d1f3b56218d7108b858645be66f03e7ebb3
Jedrzej <jbszczepaniak@gmail.com>
2017-12-06 21:27:46 +0100

    Implement ingredient that has product and amount

--- trough/main/models.py
+++ trough/main/models.py
@@ -9,6 +9,8 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=255, blank=False)
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return f'name: {self.name}, amount: {self.amount.value} {self.amount.get_units_display()}, product: {self.product.name}'
 
 class Product(models.Model):
     name = models.CharField(max_length=255, null=True)
@@ -17,11 +19,12 @@ class Product(models.Model):
 
 class Amount(models.Model):
     GRAM = 0
+    PIECES = 1
+
     UNITS_CHOICES = (
         (GRAM, 'Gram'),
+        (PIECES, 'Pieces'),
     )
     value = models.IntegerField(null=True)
-    units = models.IntegerField(null=True, choices=UNITS_CHOICES, default=GRAM)
+    units = models.IntegerField(null=True   , choices=UNITS_CHOICES, default=GRAM)
     ingredient = models.OneToOneField(Ingredient, on_delete=models.CASCADE, null=True)
-
-
--- trough/main/tests/model_test.py
+++ trough/main/tests/model_test.py
@@ -30,8 +30,6 @@ def test_ingredient_has_product_and_value():
     assert(Ingredient.objects.get(name='100 g of tuna').product == tuna)
     assert(Ingredient.objects.get(name='100 g of tuna').amount == hundred_grams)
 
-# def test_ingredient_has_product_and_amount():
-
 
 @pytest.mark.django_db
 def test_dish_can_have_many_ingredients():
@@ -39,4 +37,26 @@ def test_dish_can_have_many_ingredients():
     tuna_with_chips.save()
     tuna_with_chips.ingredient_set.create(name='tuna')
     tuna_with_chips.ingredient_set.create(name='chips')
+
+
+@pytest.mark.django_db
+def test_single_product_can_be_part_of_multiple_ingredients():
+    tuna = Product(name='tuna')
+
+    tuna_200g = Ingredient(amount=Amount(value=200, units=Amount.GRAM))
+    tuna_200g.product = tuna
+    tuna_200g.save()
+
+    tuna_100g = Ingredient(amount=Amount(value=100, units=Amount.GRAM))
+    tuna_100g.product = tuna
+    tuna_100g.save()
+
+    print(tuna_100g)
+    # print(tuna_100g.amount.value)
+
+
+
+
+
+
 
