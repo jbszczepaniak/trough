0b9be52f1bb5374bf9a60db83502ee6aa89bc35b
Jedrzej <jbszczepaniak@gmail.com>
2017-12-06 22:24:46 +0100

    Configure properly admin

--- trough/main/admin.py
+++ trough/main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from trough.main.models import Ingredient, Product, Dish, Amount
+
+
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+
+
+class DishAdmin(admin.ModelAdmin):
+    model = Dish
+    inlines = [IngredientInline]
+
+admin.site.register(Dish, DishAdmin)
+
+admin.site.register([Ingredient, Product, Amount])
--- trough/main/models.py
+++ trough/main/models.py
@@ -1,23 +1,8 @@
 from django.db import models
 
 
-class Dish(models.Model):
-    name = models.CharField(max_length=255, blank=False)
-
-
-class Ingredient(models.Model):
-    name = models.CharField(max_length=255, blank=False)
-    dish = models.ForeignKey(Dish, on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return f'name: {self.name}, ' \
-               f'amount: {self.amount.value} {self.amount.get_units_display()}, ' \
-               f'product: {self.product.name}'
-
-
 class Product(models.Model):
     name = models.CharField(max_length=255, null=True)
-    ingredient = models.OneToOneField(Ingredient, on_delete=models.CASCADE, null=True)
 
 
 class Amount(models.Model):
@@ -30,4 +15,20 @@ class Amount(models.Model):
     )
     value = models.IntegerField(null=True)
     units = models.IntegerField(null=True   , choices=UNITS_CHOICES, default=GRAM)
-    ingredient = models.OneToOneField(Ingredient, on_delete=models.CASCADE, null=True)
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    amount = models.ForeignKey(Amount, on_delete=models.CASCADE, null=True)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'name: {self.name}, ' \
+               f'amount: {self.amount.value} {self.amount.get_units_display()}, ' \
+               f'product: {self.product.name}'
+
+
+
