ce526de0c4dba990cbccbe14350c5095d66ac4b4
Jedrzej <jbszczepaniak@gmail.com>
2017-12-14 18:07:25 +0100

    Add rating fielf with constrained value

--- trough/ratings/models.py
+++ trough/ratings/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class RatingCategory(models.Model):
     name = models.CharField(max_length=255, null=True)
+
+
+class Rating(models.Model):
+    value = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(10),
+        ]
+    )
--- trough/ratings/tests/model_test.py
+++ trough/ratings/tests/model_test.py
@@ -1,8 +1,22 @@
 import pytest
+from django.core.exceptions import ValidationError
 
-from trough.ratings.models import RatingCategory
+from trough.ratings.models import RatingCategory, Rating
 
 
 @pytest.mark.django_db
 def test_rating_has_configurable_categories():
-    RatingCategory.objects.create(name='Preparation speed')
+    speed_category = RatingCategory.objects.create(name='Sweets')
+
+
+def test_rating_can_have_integer_values_from_0_to_10():
+    Rating(value=0).full_clean()
+    Rating(value=10).full_clean()
+
+    with pytest.raises(ValidationError):
+        Rating(value=20).full_clean()
+        Rating(value=-2).full_clean()
+
+
+
+
