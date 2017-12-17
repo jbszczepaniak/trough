aedbb9c58da89ad1f671847ccad5e76cedea239a
Jedrzej <jbszczepaniak@gmail.com>
2017-12-17 22:19:22 +0100

    Relate RatingCategory with Rating, add properties

--- trough/ratings/models.py
+++ trough/ratings/models.py
@@ -5,11 +5,22 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class RatingCategory(models.Model):
     name = models.CharField(max_length=255, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Rating(models.Model):
+    name = models.CharField(max_length=255)
     value = models.IntegerField(
         validators=[
             MinValueValidator(0),
             MaxValueValidator(10),
         ]
+    )
+    rating_category = models.ForeignKey(
+        RatingCategory,
+        on_delete=models.CASCADE,
+        related_name='ratings',
+        blank=True,
+        null=True
     )
--- trough/ratings/tests/model_test.py
+++ trough/ratings/tests/model_test.py
@@ -4,19 +4,25 @@ from django.core.exceptions import ValidationError
 from trough.ratings.models import RatingCategory, Rating
 
 
+def test_rating_category_is_printable():
+    rc = RatingCategory(name='Sweets')
+    assert(rc.__str__() == 'Sweets')
+
+
 @pytest.mark.django_db
-def test_rating_has_configurable_categories():
-    speed_category = RatingCategory.objects.create(name='Sweets')
+def test_rating_category_has_several_categories():
+    sweets_ratings = RatingCategory.objects.create(name='Sweets')
+    sweets_ratings.ratings.create(name='sweetness', value=10)
+    sweets_ratings.ratings.create(name='bitterness', value=0)
+
+    assert(sweets_ratings.ratings.count() == 2)
 
 
 def test_rating_can_have_integer_values_from_0_to_10():
-    Rating(value=0).full_clean()
-    Rating(value=10).full_clean()
+    Rating(name='sweetness', value=0).full_clean()
+    Rating(name='sweetness', value=10).full_clean()
 
     with pytest.raises(ValidationError):
-        Rating(value=20).full_clean()
-        Rating(value=-2).full_clean()
-
-
-
+        Rating(name='sweetness', value=20).full_clean()
+        Rating(name='sweetness', value=-2).full_clean()
 
